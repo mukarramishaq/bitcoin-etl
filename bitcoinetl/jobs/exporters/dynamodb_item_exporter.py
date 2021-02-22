99f2f9a9aee326309780f93f4f6e92c665c77e2d
Mukarram Ishaq <mukarramishaq189@gmail.com>
2021-02-22 22:14:51 +0500

    added chunk size funationality in the dynamo db exporter

--- bitcoinetl/jobs/exporters/dynamodb_item_exporter.py
+++ bitcoinetl/jobs/exporters/dynamodb_item_exporter.py
@@ -1,8 +1,12 @@
 import boto3
 from blockchainetl.jobs.exporters.in_memory_item_exporter import InMemoryItemExporter
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 class DynamoDBItemExporter:
-    def __init__(self, mongoURI="mongodb://localhost:27017/bitcoinetl"):
+    def __init__(self, uri="dynamodb://example.com/?"):
         self.db = boto3.resource('dynamodb')
+        parsed = urlparse.urlparse(uri)
+        self.chunkSize = parse_qs(parsed.query)['chunkSize']
         self.item_types = ["block", "transaction"]
         self.item_2_collection = {
             "block": "blocks",
@@ -21,9 +25,11 @@ class DynamoDBItemExporter:
         for item in items:
             item_exporter.export_item(item)
         for item_type in self.item_types:
-            with self.db.Table(self.item_2_collection[item_type]).batch_writer(overwrite_by_pkeys=self.item_2_collection_keys[item_type]) as batch:
-                for item in item_exporter.get_items(item_type):
-                    batch.put_item(item)
+            chunks = self.chunks(item_exporter.get_items(item_type), self.chunkSize or 10)
+            for chunk in chunks:
+                with self.db.Table(self.item_2_collection[item_type]).batch_writer(overwrite_by_pkeys=self.item_2_collection_keys[item_type]) as batch:
+                    for item in chunk:
+                        batch.put_item(item)
 
     def export_item(self, item):
         item_type = item.get("type", None)
@@ -34,3 +40,8 @@ class DynamoDBItemExporter:
 
     def close(self):
         pass
+
+    def chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
